840be928faf1f4228db933d2cd47c40333cde3c6
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-03-29 16:02:08 +0900

    Initial commit

---
+++ src/main/java/com/candlebe/gcoach/GcoachApplication.java
@@ -0,0 +1,13 @@
+package com.candlebe.gcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcoachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/candlebe/gcoach/GcoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candlebe.gcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
